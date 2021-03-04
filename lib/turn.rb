33c943dc14ac4530b43a2946d88f6e3ed915c2a2
Jacob Borga <jdborga@gmail.com>
2021-03-04 03:47:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,4 +36,9 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   user_input = input_to_index(user_input)
+  if user_input.between(1, 9)
+    move(board, user_input)
+  end
+  puts "Please enter a valid input"
+  turn(board)
 end
