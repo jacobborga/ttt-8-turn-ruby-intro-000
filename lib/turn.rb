668b125933bdc60955fb8f2dcd88c9cd92a7c798
Jacob Borga <jdborga@gmail.com>
2021-03-04 03:25:33 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+def display_board(board)
+  row = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " + "\n" + row + "\n" +
+       " #{board[3]} | #{board[4]} | #{board[5]} " + "\n" + row + "\n" +
+       " #{board[6]} | #{board[7]} | #{board[8]} "
+end
